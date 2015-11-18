e7abecde13ba9f829c015b2e1ed0e4eb35b0cf0a
Tiago Damascena <Tiago Damascena>
2015-11-18 11:55:49 -0200

    Adicionado o módulo Potência

---
+++ potencia/src/main/java/br/ufg/inf/es/construcao/potencia/Potencia.java
@@ -0,0 +1,41 @@
+package br.ufg.inf.es.construcao.potencia;
+
+/**
+ * Função para calculo da potencia de um numero.
+ */
+public class Potencia {
+
+    /**
+     * Calcula a potência de um número elevado a outro.
+     *
+     * @param base Base da exponenciação.
+     * @param expoente Expoente da exponenciação.
+     *
+     * @return O resultado da exponenciação.
+     *
+     * @throws IllegalArgumentException Caso a base ou 
+     * o expoente não estejam na faixa admitida.
+     * A base deve ser um valor maior que 0.
+     * O expoente não pode ser negativo.
+     */
+    public static double potencia(double base, int expoente) {
+        if (base <= 0) {
+            throw new IllegalArgumentException("base inválida");
+        }
+        
+        if (expoente < 0) {
+            throw new IllegalArgumentException("expoente inválido");
+        }
+        
+        int i = 1;
+        double potencia = 1;
+        
+        while(i <= expoente)
+        {
+            potencia = potencia * base;
+            i = i + 1;
+        }
+        
+        return potencia;
+    }
+}
---
+++ potencia/src/test/java/br/ufg/inf/es/construcao/potencia/PotenciaTest.java
@@ -0,0 +1,41 @@
+package br.ufg.inf.es.construcao.potencia;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class PotenciaTest {
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testBaseInvalida() {
+        Potencia.potencia(-1, 2);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testExpoenteInvalido() {
+        Potencia.potencia(2, -2);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testCasoDeFronteira() {
+        Potencia.potencia(0, 1);
+        Potencia.potencia(1,-1);
+    }
+
+    @Test
+     public void testCasosTriviais() throws Exception {
+        
+        assertEquals(1, Potencia.potencia(1, 2));
+        assertEquals(1, Potencia.potencia(2, 0));
+        assertEquals(2, Potencia.potencia(2, 1));
+        assertEquals(4, Potencia.potencia(2, 2));
+    }
+
+    @Test
+     public void testCasosNaoTriviais() throws Exception {
+        
+        assertEquals(25, Potencia.potencia(5, 2));
+        assertEquals(2187, Potencia.potencia(3, 7));
+    }
+    
+}
