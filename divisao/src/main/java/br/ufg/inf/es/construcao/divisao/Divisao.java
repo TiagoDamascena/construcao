40ccb1c0067782bb5c3a4edb613df614cd6295bb
Tiago Damascena <tiagobdamascena@gmail.com>
2016-01-25 14:12:41 -0200

    Módulo divisao modificado

--- divisao/src/main/java/br/ufg/inf/es/construcao/divisao/Divisao.java
+++ divisao/src/main/java/br/ufg/inf/es/construcao/divisao/Divisao.java
@@ -1,10 +1,19 @@
 package br.ufg.inf.es.construcao.divisao;
 
 /**
- *
+ * Fornece funções para divisão de inteiros
  */
 public class Divisao {
     
+    /**
+     * Realiza a divisão de um número por outro através de somas sucessivas
+     * @param divisor Divisor da divisão.
+     * @param dividendo Dividendo da divisão.
+     * @return resultado da divisão.
+     * @throws IllegalArgumentException caso um dos valores esteja fora da faixa admitida.
+     * Divisor deve ser maior ou igual a 0
+     * Dividendo deve ser maior que 0
+     */
     public static int divideSomas(int divisor, int dividendo) {
         if(divisor < 0) {
             throw new IllegalArgumentException("divisor inválido");
@@ -24,4 +33,33 @@ public class Divisao {
         
         return divisao;
     }
+    
+    /**
+     * Realiza a divisão de um número por outro através de subtrações sucessivas
+     * @param divisor Divisor da divisão.
+     * @param dividendo Dividendo da divisão.
+     * @return resultado da divisão.
+     * @throws IllegalArgumentException caso um dos valores esteja fora da faixa admitida.
+     * Divisor deve ser maior ou igual a 0
+     * Dividendo deve ser maior que 0
+     */
+    public static int divideSubtracao(int divisor, int dividendo) {
+        if(divisor < 0) {
+            throw new IllegalArgumentException("divisor inválido");
+        }
+                
+        if(dividendo <= 0) {
+            throw new IllegalArgumentException("dividendo inválido");
+        }
+        
+        int divisao = 0;
+        int soma = divisor;
+        
+        while(soma >= dividendo) {
+            soma -= dividendo;
+            divisao++;
+        }
+        
+        return divisao;
+    } 
 }
--- divisao/src/test/java/br/ufg/inf/es/construcao/divisao/DivisaoTest.java
+++ divisao/src/test/java/br/ufg/inf/es/construcao/divisao/DivisaoTest.java
@@ -9,14 +9,14 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class DivisaoTest {
-    
+    //Testes Soma
     @Test(expected=IllegalArgumentException.class)
-    public void testDivisorInvalido() {
+    public void testDivisorInvalidoSoma() {
         Divisao.divideSomas(-1, 1);
     }
     
     @Test(expected=IllegalArgumentException.class)
-    public void testDividendoInvalido() {
+    public void testDividendoInvalidoSoma() {
         Divisao.divideSomas(1, 0);
     }
     
@@ -26,5 +26,21 @@ public class DivisaoTest {
         Assert.assertEquals(5, Divisao.divideSomas(25, 5));
         Assert.assertEquals(8, Divisao.divideSomas(64, 8));
     }
+    //Testes Subtração
+    @Test(expected=IllegalArgumentException.class)
+    public void testDivisorInvalidoSubtracao() {
+        Divisao.divideSubtracao(-1, 1);
+    }
     
+    @Test(expected=IllegalArgumentException.class)
+    public void testDividendoInvalidoSubtracao() {
+        Divisao.divideSubtracao(1, 0);
+    }
+    
+    @Test
+    public void testDivideSubtracao() {
+        Assert.assertEquals(5, Divisao.divideSubtracao(10, 2));
+        Assert.assertEquals(5, Divisao.divideSubtracao(25, 5));
+        Assert.assertEquals(8, Divisao.divideSubtracao(64, 8));
+    }
 }
