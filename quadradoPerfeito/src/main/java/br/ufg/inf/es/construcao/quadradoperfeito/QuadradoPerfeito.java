12dc3cc80ae3267791671b9027fdb8a9cf774270
Tiago Damascena <tiagobdamascena@gmail.com>
2016-01-25 04:41:48 -0200

    Módulo quadradoPerfeito adicionado

---
+++ quadradoPerfeito/src/main/java/br/ufg/inf/es/construcao/quadradoperfeito/QuadradoPerfeito.java
@@ -0,0 +1,29 @@
+package br.ufg.inf.es.construcao.quadradoperfeito;
+
+/**
+ * Funções relacionadas a um quadrado perfeito
+ */
+public class QuadradoPerfeito {
+    
+    /**
+     * verifica se um número é um quadrado perfeito.
+     * @param numero numero a ser verificado.
+     * @return verdadeiro caso o numero seja um quadrado perfeito e falso caso contrário.
+     * @throws caso o número seja menor que 1.
+     */
+    public static boolean quadradoPerfeito(int numero) {
+        if(numero < 1) {
+            throw new IllegalArgumentException("numero inválido");
+        }
+        
+        int i = 1;
+        int quadrado = 1;
+        
+        while(quadrado < numero) {
+            i += 2;
+            quadrado += i;
+        }
+        
+        return (quadrado == numero);
+    }
+}
---
+++ quadradoPerfeito/src/test/java/br/ufg/inf/es/construcao/quadradoperfeito/QuadradoPerfeitoTest.java
@@ -0,0 +1,24 @@
+package br.ufg.inf.es.construcao.quadradoperfeito;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class QuadradoPerfeitoTest {
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testNumeroInvalido() {
+        QuadradoPerfeito.quadradoPerfeito(0);
+    }
+
+    @Test
+    public void testQuadradoPerfeito() {
+        Assert.assertTrue(QuadradoPerfeito.quadradoPerfeito(16));
+        Assert.assertTrue(QuadradoPerfeito.quadradoPerfeito(25));
+    }
+    
+        @Test
+    public void testNaoQuadrados() {
+        Assert.assertFalse(QuadradoPerfeito.quadradoPerfeito(10));
+        Assert.assertFalse(QuadradoPerfeito.quadradoPerfeito(72));
+    }
+}
