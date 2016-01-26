b96c8fea54bcc631035ba5060d699fd94cfd7e66
Tiago Damascena <tiagobdamascena@gmail.com>
2016-01-26 02:08:17 -0200

    Corrigido erro de teste no módulo razaoAurea

--- razaoAurea/src/test/java/br/ufg/inf/es/construcao/razaoaurea/RazaoAureaTest.java
+++ razaoAurea/src/test/java/br/ufg/inf/es/construcao/razaoaurea/RazaoAureaTest.java
@@ -17,12 +17,12 @@ public class RazaoAureaTest {
     
     @Test(expected=IllegalArgumentException.class)
     public void testFatorInvalido() {
-        RazaoAurea.razaoAurea(1,1,0);
+        RazaoAurea.razaoAurea(1,2,0);
     }
 
     @Test
     public void testRazaoAurea() {
-        assertEquals(2, RazaoAurea.razaoAurea(1,1,1), 0.0);
+        assertEquals(1.5, RazaoAurea.razaoAurea(1,2,1), 0.0);
         assertEquals(1.6, RazaoAurea.razaoAurea(1,2,3), 0.0);
     }
     
