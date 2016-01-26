60d8e4ea297aa5b09b72dda26bf09c49f3a6b04f
Tiago Damascena <tiagobdamascena@gmail.com>
2016-01-26 15:51:38 -0200

    Função modSimples adicionada ao módulo mod

--- mod/src/main/java/br/ufg/inf/es/construcao/mod/Mod.java
+++ mod/src/main/java/br/ufg/inf/es/construcao/mod/Mod.java
@@ -1,5 +1,7 @@
 package br.ufg.inf.es.construcao.mod;
 
+import br.ufg.inf.es.construcao.divisao.Divisao;
+
 /**
  * Fornece funções de cálculo de mod(resto da divisão inteira)
  */
@@ -35,5 +37,33 @@ public class Mod {
         }
         
         return soma;
+    }
+    
+    /**
+     * Calcula o mod(resto) de uma divisão
+     *
+     * @param divisor Divisor da divisão
+     * @param dividendo Dividendo da divisão
+     *
+     * @return O resto da divisão
+     *
+     * @throws IllegalArgumentException caso algum argumeto esteja fora
+     * da faixa admitida.
+     * Divisor deve ser maior que 0.
+     * Dividendo deve ser maior que 0;
+     */
+    public static int modSimples(int divisor, int dividendo) {
+        
+        if(divisor <= 0) {
+            throw new IllegalArgumentException("divisor inválido");
+        }
+        
+        if(dividendo <= 0) {
+            throw new IllegalArgumentException("dividendo inválido");
+        }
+        
+        int divisao = Divisao.divideSubtracao(divisor, dividendo);
+        
+        return divisor - (divisao * dividendo);
     }
 }
--- mod/src/test/java/br/ufg/inf/es/construcao/mod/ModTest.java
+++ mod/src/test/java/br/ufg/inf/es/construcao/mod/ModTest.java
@@ -4,7 +4,7 @@ import static junit.framework.Assert.assertEquals;
 import org.junit.Test;
 
 public class ModTest {
-    
+    //Testes Mod
     @Test(expected=IllegalArgumentException.class)
     public void testDivisorInvalido() {
         Mod.mod(0, 1);
@@ -21,5 +21,21 @@ public class ModTest {
         assertEquals(2, Mod.mod(5, 3));
         assertEquals(2, Mod.mod(30, 4));
     }
+    //Teste Mod Simples
+    @Test(expected=IllegalArgumentException.class)
+    public void testDivisorInvalidoSimples() {
+        Mod.modSimples(0, 1);
+    }
     
+    @Test(expected=IllegalArgumentException.class)
+    public void testDividendoInvalidoSimples() {
+        Mod.modSimples(1, 0);
+    }
+
+    @Test
+    public void testModSimples() {
+        assertEquals(1, Mod.modSimples(10, 3));
+        assertEquals(2, Mod.modSimples(5, 3));
+        assertEquals(2, Mod.modSimples(30, 4));
+    }
 }
