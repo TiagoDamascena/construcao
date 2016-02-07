2c9c97a5991cf674ba279d7177bfda430a413361
Tiago Damascena <tiagobdamascena@gmail.com>
2016-02-07 17:01:08 -0200

    Módulo cpf modificado

--- cpf/src/main/java/br/ufg/inf/es/construcao/cpf/Cpf.java
+++ cpf/src/main/java/br/ufg/inf/es/construcao/cpf/Cpf.java
@@ -24,4 +24,30 @@ public class Cpf {
         
         return j == d[10] && k == d[11];
     }
+    
+    public static boolean cpf2(int[] d) {
+        if (d.length != 12) {
+            throw new IllegalArgumentException("tamanho inválido");
+        }
+        
+        if (d[0] != 0) {
+            throw new IllegalArgumentException("primeiro digito inválido");
+        }
+        
+        int c = 8;
+        
+        int p = d[9];
+        int s = d[9];
+        
+        while(c >= 1) {
+            p += d[c];
+            s += p;
+            c--;
+        }
+        
+        int j = Mod.mod(Mod.mod(s, 11), 10);
+        int k = Mod.mod(Mod.mod(s - p + (9 * j), 11), 10);
+        
+        return j == d[10] && k == d[11];
+    }
 }
--- cpf/src/test/java/br/ufg/inf/es/construcao/cpf/CpfTest.java
+++ cpf/src/test/java/br/ufg/inf/es/construcao/cpf/CpfTest.java
@@ -33,4 +33,32 @@ public class CpfTest {
         Assert.assertFalse(Cpf.cpf(vetor2));
     }
     
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testTamanhoInvalido2() {
+        int[] vetor = new int[13];
+        Cpf.cpf2(vetor);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testPrimeiroDigitoInvalido2() {
+        int[] vetor = new int[13];
+        vetor[0] = 1;
+        Cpf.cpf2(vetor);
+    }
+    
+    @Test
+    public void testCpfTrue2() {
+        int[] vetor = {0, 6, 4, 8, 3, 7, 8, 7, 6, 8, 5, 4};
+        Assert.assertTrue(Cpf.cpf2(vetor));
+
+        int[] vetor2 = {0, 7, 6, 6, 0, 1, 5, 5, 2, 0, 2, 0};
+        Assert.assertTrue(Cpf.cpf2(vetor2));
+    }
+    
+    @Test
+    public void testCpfFalse2() {        
+        int[] vetor2 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2};
+        Assert.assertFalse(Cpf.cpf2(vetor2));
+    }
 }
